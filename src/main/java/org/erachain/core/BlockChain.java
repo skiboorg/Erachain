b9c48ff866dd5f302c4b53f33a81f8c672f7e00e
icreator <icreator@mail.ru>
2020-10-04 11:25:27 +0300

    getBonusFee - 0

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -1143,7 +1143,11 @@ public class BlockChain {
             repeatsMin = (repeatsMin >> 2);
 
             if (repeatsMin < REPEAT_WIN) {
+                if (height < ALL_VALID_BEFORE) {
+                    repeatsMin = 1;
+                } else {
                     repeatsMin = REPEAT_WIN;
+                }
             }
         }
 
