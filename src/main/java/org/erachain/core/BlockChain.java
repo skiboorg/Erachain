93a8a6e3ae0c0dbbca65399619a42b92a0a07048
icreator <icreator@mail.ru>
2021-04-21 12:59:19 +0300

    allValidBefore = 0

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -169,9 +169,9 @@ public class BlockChain {
     public static final int MIN_GENERATING_BALANCE = 100000;
     public static final BigDecimal MIN_GENERATING_BALANCE_BD = new BigDecimal(MIN_GENERATING_BALANCE);
 
-    public static final int MIN_REGISTERING_BALANCE_OWN = 0;
+    public static final int MIN_REGISTERING_BALANCE_OWN = 100000;
     public static final BigDecimal MIN_REGISTERING_BALANCE_OWN_BD = new BigDecimal(MIN_REGISTERING_BALANCE_OWN);
-    public static final int MIN_REGISTERING_BALANCE_USE = 0;
+    public static final int MIN_REGISTERING_BALANCE_USE = 100000;
     public static final BigDecimal MIN_REGISTERING_BALANCE_USE_BD = new BigDecimal(MIN_REGISTERING_BALANCE_USE);
 
     public static final int MIN_REGISTERING_BALANCE_10 = 10;
@@ -802,22 +802,6 @@ public class BlockChain {
 
     public static int VALID_PERSON_REG_ERA(Transaction transaction, int height, BigDecimal totalERA, BigDecimal totalLIA) {
 
-        if (START_ISSUE_RIGHTS > 0 && height < START_ISSUE_RIGHTS) {
-            return 0;
-        }
-
-        if (totalLIA.compareTo(BigDecimal.TEN) < 0) {
-            ;
-        } else if (totalLIA.compareTo(new BigDecimal("20")) < 0) {
-            if (totalERA.compareTo(MIN_REGISTERING_BALANCE_100_BD) < 0) {
-                return Transaction.NOT_ENOUGH_ERA_OWN_100;
-            }
-        } else {
-            if (totalERA.compareTo(MIN_REGISTERING_BALANCE_1000_BD) < 0) {
-                return Transaction.NOT_ENOUGH_ERA_OWN_1000;
-            }
-        }
-
         if (MIN_REGISTERING_BALANCE_OWN > 0 && totalERA.compareTo(MIN_REGISTERING_BALANCE_OWN_BD) < 0) {
             transaction.setErrorValue("balance in OWN less then " + MIN_REGISTERING_BALANCE_OWN);
             return Transaction.NOT_ENOUGH_ERA_OWN;
@@ -832,25 +816,6 @@ public class BlockChain {
     }
 
     public static int VALID_PERSON_CERT_ERA(Transaction transaction, int height, BigDecimal totalERA, BigDecimal totalLIA) {
-
-        if (START_ISSUE_RIGHTS > 0 && height < START_ISSUE_RIGHTS) {
-            if (totalERA.compareTo(MIN_REGISTERING_BALANCE_100_BD) < 0) {
-                return Transaction.NOT_ENOUGH_ERA_OWN_100;
-            }
-            return 0;
-        }
-
-        if (totalLIA.compareTo(BigDecimal.TEN) < 0) {
-            ;
-        } else if (totalLIA.compareTo(new BigDecimal("20")) < 0) {
-            if (totalERA.compareTo(MIN_REGISTERING_BALANCE_100_BD) < 0) {
-                return Transaction.NOT_ENOUGH_ERA_OWN_100;
-            }
-        } else {
-            if (totalERA.compareTo(MIN_REGISTERING_BALANCE_1000_BD) < 0) {
-                return Transaction.NOT_ENOUGH_ERA_OWN_1000;
-            }
-        }
 
         if (MIN_REGISTERING_BALANCE_OWN > 0 && totalERA.compareTo(MIN_REGISTERING_BALANCE_OWN_BD) < 0) {
             transaction.setErrorValue("balance in OWN less then " + MIN_REGISTERING_BALANCE_OWN);
