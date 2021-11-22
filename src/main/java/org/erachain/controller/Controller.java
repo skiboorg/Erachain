d088140666a058ebc4eb44a17cc6b577cbb7ec57
icreator <icreator@mail.ru>
2021-11-22 10:01:16 +0300

    03 - anonime denied assets

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -101,7 +101,7 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "2021.11 02";
+    public static String version = "2021.11 03";
     public static String buildTime = "2021-10-16 12:00:00 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
