63d8e72de5ad2b4f7568d6cc31fb99b237a7c66b
icreator <icreator@mail.ru>
2022-05-23 15:44:14 +0300

    RU PAY

--- src/main/java/org/erachain/settings/Settings.java
+++ src/main/java/org/erachain/settings/Settings.java
@@ -32,9 +32,9 @@ public class Settings {
     public static final long DEFAULT_MAINNET_STAMP = 1620046800000L; // MAIN Net
     public static final long DEFAULT_DEMO_NET_STAMP = 1622206800000L; // DEMO Net
 
-    public static String FORK_APP_Name = "Foil Network";
-    public static String FORK_APP_URL_Name = "Foil.Network";
-    public static String FORK_APP_URL = "https://foil.network";
+    public static String FORK_APP_Name = "RU PAY";
+    public static String FORK_APP_URL_Name = "RuPay.Pro";
+    public static String FORK_APP_URL = "https://rupay.pro";
     public static String FORK_APP_NAME = FORK_APP_Name.toUpperCase();
     public static String FORK_APP_name = FORK_APP_Name.toLowerCase();
 
