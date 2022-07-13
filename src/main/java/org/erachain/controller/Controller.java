66b79f4725a1c68d22540e077b4198a19f23db94
icreator <icreator@mail.ru>
2022-07-13 19:15:43 +0300

    USDT

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -100,7 +100,7 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "2022.06 02";
+    public static String version = "2022.07 01";
     public static String buildTime = "2022-05-28 12:00:00 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
